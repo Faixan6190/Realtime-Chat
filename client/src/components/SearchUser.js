c6bc2bbd7c97b522a73178a4b119f9ac90ade80b
Faixan6190 <faixan6190@gmail.com>
2024-05-26 02:16:26 +0500

    add new file'

--- client/src/components/SearchUser.js
+++ client/src/components/SearchUser.js
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import { IoSearchOutline } from "react-icons/io5";
 
 const SearchUser = () => {
-  return <div>SearchUser</div>;
+  const [searchUser, setSearchUser] = useState([]);
+  const [loading, setLoading] = useState(true);
+  return (
+    <div className="fixed top-0 right-0 bottom-0 left-0 bg-slate-700 bg-opacity-40 p-2">
+      <div className="w-full max-w-lg mx-auto mt-10">
+        {/* input search user */}
+        <div className="bg-white rounded h-14 overflow-hidden flex">
+          <input className="w-full outline-none py-1 h-full px-4" type="text" placeholder="Search user by name, email...." />
+          <div className="h-14 w-14 flex justify-center items-center">
+            <IoSearchOutline size={25} />
+          </div>
+        </div>
+        {/* display search user */}
+        <div className="bg-white mt-2 w-full p-4 rounded">
+          {/* no user found */}
+          {searchUser.length === 0 && !loading && <p className="text-center text-slate-500">No User Found!</p>}
+          {loading && <p>loading...</p>}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SearchUser;
