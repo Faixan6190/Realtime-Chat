48dc2653429b7ffd68f2ff78be38bfacc94f47e4
Faixan6190 <faixan6190@gmail.com>
2024-05-27 21:58:42 +0500

    update ui

--- server/socket/index.js
+++ server/socket/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import { Server } from "socket.io";
 import http from "http";
 import getUserDetailsFromToken from "../helpers/getUserDetailsFromToken.js";
+import UserModel from "../models/UserModel.js";
 
 const app = express();
 
@@ -16,7 +17,7 @@ const io = new Server(server, {
 //online user
 const onlineUser = new Set();
 io.on("connection", async (socket) => {
-  // console.log("connected user", socket.id);
+  console.log("connected user", socket.id);
   const token = socket.handshake.auth.token;
   // current user details
   const user = await getUserDetailsFromToken(token);
@@ -24,10 +25,21 @@ io.on("connection", async (socket) => {
   socket.join(user?._id);
   onlineUser.add(user?._id);
   io.emit("onlineUser", Array.from(onlineUser));
+  socket.on("message-page", async (userId) => {
+    console.log("userId", userId);
+    const userDetails = await UserModel.findById(userId).select("-password");
+    const payload = {
+      _id: userDetails?._id,
+      name: userDetails?.name,
+      email: userDetails?.email,
+      online: onlineUser.has(userId),
+    };
+    socket.emit("message-user", payload);
+  });
   //disconnect
   socket.on("disconnect", () => {
     onlineUser.delete(user?._id);
-    // console.log("disconnect user", socket.id);
+    console.log("disconnect user", socket.id);
   });
 });
 
