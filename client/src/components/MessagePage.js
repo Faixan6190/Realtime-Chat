3f754f24528337779c5447221e7bdbacd86b1ea1
Faixan6190 <faixan6190@gmail.com>
2024-05-29 23:43:58 +0500

    message page complete'

--- client/src/components/MessagePage.js
+++ client/src/components/MessagePage.js
@@ -196,7 +196,7 @@ const MessagePage = () => {
         <div className="flex flex-col gap-2 py-2 mx-2" ref={currentMessage}>
           {allMessage.map((msg, index) => {
             return (
-              <div className={`bg-white p-1 py-1 rounded w-fit ${user._id === msg.msgByUserId ? "ml-auto bg-teal-100" : "s"}`}>
+              <div className={`bg-white p-1 py-1 rounded w-fit ${user._id === msg.msgByUserId ? "ml-auto bg-teal-100" : ""}`}>
                 <p className="px-2">{msg.text}</p>
                 <p className="text-xs ml-auto w-fit">{moment(msg.createdAt).format("hh:mm")}</p>
               </div>
